9fce6acfb42ebdec1b0dfb226c430dbd4103a542
Avishka-Dulanjana <avishkadulanjana98@gmail.com>
2024-02-14 03:12:17 +0530

    Task_magnet_pro_v1.4

--- task_magnet_pro_api/server.py
+++
@@ -1,4 +0,0 @@
-from src import app
-
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
--- task_magnet_pro_api/src/models/userModel.py
+++
@@ -1,6 +0,0 @@
-class User:
-    def __init__(self, username, password, email, role):
-        self.username = username
-        self.password = password
-        self.email = email
-        self.role = role
