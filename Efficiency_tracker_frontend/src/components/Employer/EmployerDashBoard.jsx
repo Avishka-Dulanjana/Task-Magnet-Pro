06d50971c67fd90f2fe22d7c73a27038084b2b4c
RashindiPaulusha99 <rashindipaulusha837@gmail.com>
2024-02-29 22:43:30 +0530

    added employee header and dashboard page

---
+++ Efficiency_tracker_frontend/src/components/Employer/EmployerDashBoard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const EmployerDashBoard = ({ children }) => {
+  return (
+    
+  );
+};
+
+export default EmployerDashBoard;
---
+++ Efficiency_tracker_frontend/src/components/Employer/EmployerHeader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const EmployerHeader = () => {
+
+  
+  return (
+    
+  );
+};
+
+export default EmployerHeader;
