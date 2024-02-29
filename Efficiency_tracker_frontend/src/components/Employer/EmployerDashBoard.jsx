c8c26c8b43ba2b8be3d9d936eb3b28772252f017
DonShasindu <shasinduthathsara02@gmail.com>
2024-03-01 00:19:54 +0530

    created EmployerDashboard

--- Efficiency_tracker_frontend/src/components/Employer/EmployerDashBoard.jsx
+++ Efficiency_tracker_frontend/src/components/Employer/EmployerDashBoard.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-
+import EmployerHeader from "./EmployerHeader";
+import Footer from "../Footer";
 
 const EmployerDashBoard = ({ children }) => {
   return (
-    
+    <div className="h-screen">
+      <EmployerHeader />
+      <div className="h-screen">{children}</div>
+      <div className="fixed bottom-0 w-full">
+        <Footer />
+      </div>
+    </div>
   );
 };
 
