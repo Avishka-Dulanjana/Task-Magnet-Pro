f3efa885726bce2d993358c2588b823f1e7f0e38
Upeksha1214 <upekshasachintha@gmail.com>
2024-02-29 09:51:24 -0800

    Login component is done

--- Efficiency_tracker_frontend/src/components/Login.jsx
+++ Efficiency_tracker_frontend/src/components/Login.jsx
@@ -1,12 +1,45 @@
 import React, { useState } from "react";
-
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { baseURL } from "../apiURL";
+import { useSnackbar } from "notistack";
 
 const Login = () => {
-  
+  const [loginData, setLoginData] = useState({});
+  const { enqueueSnackbar } = useSnackbar();
+  const navigate = useNavigate();
 
-  const login = async () => {
+  function handleChange(event) {
+    setLoginData({
+      ...loginData,
+      [event.target.name]: event.target.value,
+    });
   }
 
+  const login = async () => {
+    const data = loginData;
+    await axios
+      .post(`${baseURL}/auth/login`, data)
+      .then((res) => {
+        if (res.status === 200) {
+          localStorage.setItem("role", res.data.role);
+          localStorage.setItem("user", res.data._id);
+          if (res.data.role === "employer") {
+            navigate("/dashboard-employer");
+          } else if (res.data.role === "employee") {
+            navigate("/dashboard-employee");
+          }else{
+            navigate("/");
+          }
+        }
+      })
+      .catch((err) => {
+        enqueueSnackbar("Login failed.Try again", {
+          variant: "error",
+        });
+        console.log(err.response);
+      });
+  };
   return (
     <div className="bg-gray-10 ">
       <div className="flex justify-center h-screen w-screen items-center">
@@ -21,7 +54,7 @@ const Login = () => {
               id="username"
               className="w-full py-4 px-8 bg-slate-200 placeholder:font-semibold rounded hover:ring-1 outline-blue-500"
               placeholder="Name or User Name"
-              
+              onChange={handleChange}
             />
           </div>
           <div className="w-3/4 mb-6">
@@ -31,7 +64,7 @@ const Login = () => {
               id="password"
               className="w-full py-4 px-8 bg-slate-200 placeholder:font-semibold rounded hover:ring-1 outline-blue-500 "
               placeholder="Password"
-              
+              onChange={handleChange}
             />
           </div>
           <div className="w-3/4 flex flex-row justify-between"></div>
@@ -40,7 +73,7 @@ const Login = () => {
             <button
               type="button"
               className="py-4  w-full rounded text-blue-50 font-bold bg-blue-700 hover:bg-blue-500"
-              
+              onClick={() => login()}
             >
               {" "}
               LOGIN
@@ -60,7 +93,6 @@ const Login = () => {
       </div>
     </div>
   );
-   
 };
 
 export default Login;
