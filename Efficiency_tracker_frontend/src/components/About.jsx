ce71c5b3a971dcff3fbb243c15345cd3bb7f5223
Avishka-Dulanjana <avishkadulanjana98@gmail.com>
2024-03-01 08:58:38 +0530

    Created About screen

---
+++ Efficiency_tracker_frontend/src/components/About.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="bg-gray-100">
+      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+        <h2 className="text-3xl font-extrabold text-gray-900">
+          About Efficiency Tracker
+        </h2>
+        <p className="mt-4 text-lg text-gray-600">
+          As a ongoing start-up business in recent times , we envision the
+          intelligent upgrade of the WORKING FROM HOME SYSTEM; Consequently
+          inspiring World towards matching global standers in effective problem
+          solving via technology.{" "}
+        </p>
+        <p className="mt-4 text-lg text-gray-600">
+          The EfficiencyTracker is a platform that facilitates a real time
+          connection between the remote employees and the their employers
+          ,enabling mutual engagement for the delivering the good service and
+          manage the time effectively{" "}
+        </p>
+        <p className="mt-4 text-lg text-gray-600">
+          Efficiency Tracker is the ideal solution for organizations of all
+          sizes and industries, from small startups to large corporations.
+          Whether you're looking to improve employee productivity, optimize
+          business processes, or gain a better understanding of how your
+          employees are performing, Efficiency Tracker can help you achieve your
+          goals.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
