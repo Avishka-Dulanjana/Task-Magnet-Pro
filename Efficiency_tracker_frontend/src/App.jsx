c6d0061723b1edcafcabd23e468d522ad0db7958
RashindiPaulusha99 <rashindipaulusha837@gmail.com>
2024-03-01 09:24:42 +0530

    added display employee list in employer dashboard

--- Efficiency_tracker_frontend/src/App.jsx
+++ Efficiency_tracker_frontend/src/App.jsx
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { SnackbarProvider } from "notistack";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import About from "./components/About";
+import About from "./components/About";
 // import EmployeeDashBoard from "../components/Employee/EmployeeDashBoard";
 // import MyTask from "./components/Employee/MyTask";
 // import Profile from "./components/Employee/Profile";
 // import SubmitTask from "./components/Employee/SubmitTask";
 // import AllTasks from "./components/Employer/AllTasks";
-// import CreateTask from "./components/Employer/CreateTask";
-// import EmployeeList from "./components/Employer/EmployeeList";
+import CreateTask from "./components/Employer/CreateTask";
+import EmployeeList from "./components/Employer/EmployeeList";
 import EmployerDashBoard from "./components/Employer/EmployerDashBoard";
 import WelcomeNote from "./components/Employer/WelcomeNote";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
-// import Contact from "./components/Contact";
+import Contact from "./components/Contact";
 
 function App() {
   // const [currentUserRole, setCurrentUserRole] = useState(
@@ -63,7 +63,7 @@ function App() {
                     <EmployerDashBoard
                       children={
                         <>
-                          {/* <CreateTask /> */}
+                          <CreateTask />
                         </>
                       }
                     />
@@ -87,20 +87,20 @@ function App() {
                     <EmployerDashBoard
                       children={
                         <>
-                          {/* <EmployeeList /> */}
+                          <EmployeeList />
                         </>
                       }
                     />
                   }
                 />
-                {/* <Route
+                <Route
                   path="/about"
                   element={<EmployerDashBoard children={<About />} />}
                 />
                 <Route
                   path="/contact"
                   element={<EmployerDashBoard children={<Contact />} />}
-                /> */}
+                />
               </>
             {/* )} */}
             {/* {currentUserRole === "Employee" && ( */}
