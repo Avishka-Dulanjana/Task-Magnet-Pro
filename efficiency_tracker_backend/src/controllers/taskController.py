f6a4b61ef8fa88513def6a0f58e5db6d77c4e6a7
RashindiPaulusha99 <rashindipaulusha837@gmail.com>
2024-03-16 13:28:30 +0530

    added start, pause, stop method

--- efficiency_tracker_backend/src/controllers/taskController.py
+++ efficiency_tracker_backend/src/controllers/taskController.py
@@ -49,6 +49,62 @@ def find_by_status():
 
     return json.dumps(result, default=str)
 
+@taskCtrl.route('/start', methods=['POST'])
+def start_task():
+    data = request.json
+
+    result = db.tasks.update_one({'_id': ObjectId(data['id'])}, {
+        '$set': {'startTime': data['startTime'], 'lastStartTime': data['startTime'], 'isTaskStart': True}})
+
+    return json.dumps({'acknowledged': result.acknowledged}, default=str)
+
+
+@taskCtrl.route('/start_pause', methods=['POST'])
+def start_pause_task():
+    data = request.json
+
+    result = db.tasks.update_one({'_id': ObjectId(data['id'])}, {
+        '$set': {'lastStartTime': data['startTime'], 'isPause': False}})
+
+    return json.dumps({'acknowledged': result.acknowledged}, default=str)
+
+
+@taskCtrl.route('/pause', methods=['POST'])
+def pause_task():
+    data = request.json
+
+    doc = db.tasks.find_one({'_id': ObjectId(data["id"])})
+
+    startTime = datetime.strptime(doc['lastStartTime'], "%Y-%m-%d %H:%M:%S")
+    pauseTime = datetime.strptime(data['pauseTime'], "%Y-%m-%d %H:%M:%S")
+
+    time_diff = ((pauseTime - startTime).total_seconds()) + \
+        doc['spendTime']
+
+    result = db.tasks.update_one({'_id': ObjectId(data['id'])}, {
+        '$set': {'spendTime': time_diff, 'isPause': True}})
+
+    return json.dumps({'acknowledged': result.acknowledged}, default=str)
+
+
+@taskCtrl.route('/end', methods=['POST'])
+def end_task():
+    data = request.json
+
+    doc = db.tasks.find_one({'_id': ObjectId(data["id"])})
+
+    startTime = datetime.strptime(doc['lastStartTime'], "%Y-%m-%d %H:%M:%S")
+    endTime = datetime.strptime(data['endTime'], "%Y-%m-%d %H:%M:%S")
+
+    time_diff = ((endTime - startTime).total_seconds()) + \
+        doc['spendTime']
+
+    result = db.tasks.update_one({'_id': ObjectId(data['id'])}, {
+        '$set': {'spendTime': time_diff, 'endTime': endTime, 'isPause': False, 'isTaskStart': False, 'isTaskComplete': True}})
+
+    return json.dumps({'acknowledged': result.acknowledged}, default=str)
+
+
 # search tasks by user
 @taskCtrl.route('/find_by_user', methods=['GET'])
 def find_by_user():
